8d79596af51e9b3763079127f54ebdeee431c980
Mikhail_Lobov <qwe>
2022-07-18 22:23:47 +0300

    - еще немного объектных пулов

--- JoyWay-Test-Lobov-2022/Assets/Game/Scripts/ObjectPool/ObjectPoolManager.cs
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/ObjectPool/ObjectPoolManager.cs
@@ -61,6 +61,49 @@ namespace Game.Scripts.ObjectPool
                     break;
             }
         }
+
+        public PickupItemController GetPickupObject(ItemType itemType)
+        {
+            GameObject obj = null;
+
+            switch (itemType)
+            {
+                case ItemType.Pistol:
+                    obj = _pistolPickupPool.GetObject();
+                    break;
+                
+                case ItemType.FireStone:
+                    obj = _fireStonePickupPool.GetObject();
+                    break;
+                
+                case ItemType.WaterStone:
+                    obj = _waterStonePickupPool.GetObject();
+                    break;
+            }
+
+            var controller = obj.GetComponent<PickupItemController>();
+            return controller;
+        }
+
+        public void ReturnPickupObject(PickupItemController pickupItemController)
+        {
+            var itemType = pickupItemController.ItemType;
+
+            switch (itemType)
+            {
+                case ItemType.Pistol:
+                    _pistolPickupPool.ReturnObject(pickupItemController.gameObject);
+                    break;
+                
+                case ItemType.FireStone:
+                    _fireStonePickupPool.ReturnObject(pickupItemController.gameObject);
+                    break;
+                
+                case ItemType.WaterStone:
+                    _waterStonePickupPool.ReturnObject(pickupItemController.gameObject);
+                    break;
+            }
+        }
     }
 }
 
