9ad961c0ca6e88f714e7f17de7dd17aa48315623
Mikhail_Lobov <qwe>
2022-07-18 17:17:27 +0300

    - сделал класс под теги

---
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Utils/Tags.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Utils
+{
+    public class Tags
+    {
+        public static string Ground = "Ground";
+    }
+}
+
