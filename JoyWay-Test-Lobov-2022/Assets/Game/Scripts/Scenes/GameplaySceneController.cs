fa2942a93f026f5e8fa16fd617d29b1fde52257a
Mikhail_Lobov <qwe>
2022-07-19 20:06:44 +0300

    - спавн игровых объектов

--- JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Scenes/GameplaySceneController.cs
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Scenes/GameplaySceneController.cs
@@ -35,11 +35,7 @@ namespace Game.Scripts.Scenes
             
             _objectPoolManager.OnAwake();
 
-            var player = _objectPoolManager.GetCreatureObject(CreatureType.Player);
-            player.Transform.position = _playerSpawnPoint.position;
-
-            var dummy = _objectPoolManager.GetCreatureObject(CreatureType.Dummy);
-            dummy.Transform.position = _dummySpawnPoint.position;
+            SpawnGameObjects();
         }
 
         private void Start()
@@ -73,6 +69,49 @@ namespace Game.Scripts.Scenes
             
             // game systems
             _diContainer.BindInstance(CreatureSystem.Instance);
+        }
+
+        private void SpawnGameObjects()
+        {
+            var player = _objectPoolManager.GetCreatureObject(CreatureType.Player);
+            player.Transform.position = _playerSpawnPoint.position;
+            player.Transform.eulerAngles = _playerSpawnPoint.eulerAngles;
+            player.gameObject.SetActive(true);
+
+            var dummy = _objectPoolManager.GetCreatureObject(CreatureType.Dummy);
+            dummy.Transform.position = _dummySpawnPoint.position;
+            dummy.Transform.eulerAngles = _dummySpawnPoint.eulerAngles;
+            dummy.gameObject.SetActive(true);
+
+            for (int i = 0; i < _pistolPickupSpawnPoints.Length; i++)
+            {
+                var spawnPoint = _pistolPickupSpawnPoints[i];
+                
+                var pistolPickup = _objectPoolManager.GetPickupObject(ItemType.Pistol);
+                pistolPickup.gameObject.SetActive(true);
+                pistolPickup.transform.position = spawnPoint.position;
+                pistolPickup.transform.eulerAngles = spawnPoint.eulerAngles;
+            }
+            
+            for (int i = 0; i < _fireStonePickupSpawnPoints.Length; i++)
+            {
+                var spawnPoint = _fireStonePickupSpawnPoints[i];
+                
+                var fireStonePickup = _objectPoolManager.GetPickupObject(ItemType.FireStone);
+                fireStonePickup.gameObject.SetActive(true);
+                fireStonePickup.transform.position = spawnPoint.position;
+                fireStonePickup.transform.eulerAngles = spawnPoint.eulerAngles;
+            }
+            
+            for (int i = 0; i < _waterStonePickupSpawnPoints.Length; i++)
+            {
+                var spawnPoint = _waterStonePickupSpawnPoints[i];
+                
+                var waterStonePickup = _objectPoolManager.GetPickupObject(ItemType.WaterStone);
+                waterStonePickup.gameObject.SetActive(true);
+                waterStonePickup.transform.position = spawnPoint.position;
+                waterStonePickup.transform.eulerAngles = spawnPoint.eulerAngles;
+            }
         }
     }
 }
