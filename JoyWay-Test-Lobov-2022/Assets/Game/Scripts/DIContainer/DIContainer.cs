e2a4b3042612e558f173d26aa32d4ca67f0aebbd
Mikhail_Lobov <qwe>
2022-07-19 13:51:44 +0300

    - набросал небольшой DI контейнер

---
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/DIContainer/DIContainer.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZerglingPlugins.Tools.Log;
+using ZerglingPlugins.Tools.Singleton;
+
+namespace Game.Scripts.DIContainer
+{
+    public class DIContainer : Singleton<DIContainer>
+    {
+        private Dictionary<Type, object> _bindings;
+
+        public DIContainer(): base()
+        {
+            _bindings = new Dictionary<Type, object>();
+        }
+
+        public void BindInstance(object instance)
+        {
+            var instanceType = instance.GetType();
+            if (_bindings.ContainsKey(instanceType))
+            {
+                LogUtils.Error(this, $"{instanceType} is already binded!");
+                return;
+            }
+
+            _bindings[instanceType] = instance;
+        }
+
+        public void UnBindInstance(object instance)
+        {
+            var instanceType = instance.GetType();
+            if (!_bindings.ContainsKey(instanceType))
+            {
+                LogUtils.Error(this, $"Therer are no instances of {instanceType}");
+                return;
+            }
+
+            _bindings.Remove(instanceType);
+        }
+
+        public T Resolve<T>() where T : class, new()
+        {
+            var instanceType = typeof(T);
+            if (!_bindings.ContainsKey(instanceType))
+            {
+                LogUtils.Error(this, $"{instanceType} has no binded instances!");
+                return null;
+            }
+
+            T result = (T) _bindings[instanceType];
+            return result;
+        }
+    }
+}
+
+
