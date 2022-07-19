259aafa9b04e656d3e41054af056bb0f244c402b
Mikhail_Lobov <qwe>
2022-07-19 22:34:49 +0300

    - собрал префабы предметов в руку и сделал заготовки классов

---
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Items/HandItems/HandItemController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Game.Scripts.Creatures.Player;
+using Game.Scripts.DI;
+using Game.Scripts.Enums;
+using Game.Scripts.Events;
+using Game.Scripts.MonoBehaviours;
+using Game.Scripts.ObjectPool;
+using UnityEditor.Profiling.Memory.Experimental;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Scripts.Items
+{
+    public class HandItemController : BasicMonoBehaviour, IObjectPoolItem
+    {
+        [SerializeField] private ItemId _itemId;
+
+        private PlayerController _playerController;
+
+        private ObjectPoolManager _objectPoolManager;
+
+        public void OnSpawn()
+        {
+        }
+
+        public void OnDespawn()
+        {
+        }
+        
+        public virtual void Setup(PlayerController playerController)
+        {
+            _playerController = playerController;
+            
+            var diContainer = DIContainer.Instance;
+            _objectPoolManager = diContainer.Resolve<ObjectPoolManager>();
+        }
+
+        public virtual void Fire()
+        {
+        }
+
+        public void Drop()
+        {
+            
+        }
+    }
+}
+
