5e8954fd7b9103147ffc1dbb2c56deda90ac3ded
Mikhail_Lobov <qwe>
2022-07-20 19:00:18 +0300

    - сделал енам для типа урона

---
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Enums/Damage.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Enums
+{
+    public enum DamageType
+    {
+        NONE = 0,
+        Physical = 1,
+        Fire = 2,
+        Water = 3,
+    }
+}
+
