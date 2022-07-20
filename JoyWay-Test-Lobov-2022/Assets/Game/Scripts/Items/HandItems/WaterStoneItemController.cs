c694f0f7a1836bbcd805bd482535358d443dd562
Mikhail_Lobov <qwe>
2022-07-20 22:40:52 +0300

    - стрельба прожектайлами водного камня

--- JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Items/HandItems/WaterStoneItemController.cs
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Items/HandItems/WaterStoneItemController.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Game.Scripts.Damage;
+using Game.Scripts.Utils;
 using UnityEngine;
 
 namespace Game.Scripts.Items
 {
     public class WaterStoneItemController : HandItemController
     {
+        [SerializeField] private Transform _projectileSpawnPoint;
+        [SerializeField] private float _cameraForwardFactor;
+        
+        protected override void UsePerformed()
+        {
+            var cameraPosition = _playerController.CameraTransform.position;
+            var cameraForward = _playerController.CameraTransform.forward;
+            var hitPosition = cameraPosition + cameraForward * _cameraForwardFactor;
+            var forceVector = hitPosition - _projectileSpawnPoint.position;
+
+            var damage = new DamageStruct();
+            damage.DamageType = _config.damageType;
+            damage.DamageValue = _config.damageValue;
+            damage.WetValue = _config.wetValue;
+
+            var projectile = _objectPoolManager.GetProjectileObject(_config.projetileType);
+            projectile.Setup(damage);
+            projectile.Transform.position = _projectileSpawnPoint.position;
+            projectile.SetActive(true);
+            projectile.AddForce(forceVector, ForceMode.Impulse);
+        }
     }
 }
 
