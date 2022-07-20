d9bae485fb7b7393842fff16c5759054957441d0
Mikhail_Lobov <qwe>
2022-07-21 00:10:26 +0300

    - добавил партикл систему и сделал атаку огненным камнем

--- JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Items/HandItems/FireStoneItemController.cs
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Items/HandItems/FireStoneItemController.cs
@@ -1,11 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Game.Scripts.Creatures.Player;
 using UnityEngine;
+using ZerglingPlugins.Tools.Log;
 
 namespace Game.Scripts.Items
 {
     public class FireStoneItemController : HandItemController
     {
+        [SerializeField] private ParticleSystem _particleSystem;
+
+        protected override void UsePerformed()
+        {
+            var emission = _particleSystem.emission;
+            emission.enabled = true;
+            _particleSystem.Play();
+        }
+
+        protected override void UseCanceled()
+        {
+            var emission = _particleSystem.emission;
+            emission.enabled = false;
+        }
+
+        private void OnParticleCollision(GameObject other)
+        {
+            LogUtils.Info(this, "OnParticleCollision");
+        }
     }
 }
 
