9738e52e55eb21c551a1d4c81a50e80425908fc3
Mikhail_Lobov <qwe>
2022-07-20 22:34:52 +0300

    - простая логика прожектайлов

---
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Enums/Projectiles.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Enums
+{
+    public enum ProjectileType
+    {
+        NONE = 0,
+        WaterStone = 1,
+    }
+}
---
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Projectiles/ProjectileController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Game.Scripts.Creatures.Basic;
+using Game.Scripts.Damage;
+using Game.Scripts.DI;
+using Game.Scripts.Enums;
+using Game.Scripts.MonoBehaviours;
+using Game.Scripts.ObjectPool;
+using Game.Scripts.Utils;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using ZerglingPlugins.Tools.Log;
+
+namespace Game.Scripts.Projectiles
+{
+    public class ProjectileController : BasicMonoBehaviour, IObjectPoolItem
+    {
+        public ProjectileType ProjectileType => _projectileType;
+        
+        [SerializeField] private ProjectileType _projectileType;
+        [SerializeField] private Rigidbody _rigidbody;
+        
+        private DamageStruct _damageStruct;
+        
+        private ObjectPoolManager _objectPoolManager;
+
+        public void Setup(DamageStruct damageStruct)
+        {
+            _damageStruct = damageStruct;
+        }
+
+        public void OnSpawn()
+        {
+            var diContainer = DIContainer.Instance;
+            _objectPoolManager = diContainer.Resolve<ObjectPoolManager>();
+        }
+
+        public void OnDespawn()
+        {
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.angularVelocity = Vector3.zero;
+        }
+        
+        public void AddForce(Vector3 force, ForceMode forceMode)
+        {
+            _rigidbody.AddForce(force, forceMode);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            _objectPoolManager.ReturnProjectileObject(this);
+            if (!collision.gameObject.tag.Equals(Tags.Creature))
+                return;
+
+            var controller = collision.collider.gameObject.GetComponent<CreatureController>();
+            controller.ApplyDamage(_damageStruct);
+        }
+    }
+}
+
