0f1a3f9d32ade9794dec9bffdcae3938b47ead5e
Mikhail_Lobov <qwe>
2022-07-21 13:23:38 +0300

    - эвент на изменение значений существа

--- JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Events/Creatures.cs
+++ JoyWay-Test-Lobov-2022/Assets/Game/Scripts/Events/Creatures.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 
 namespace Game.Scripts.Events
 {
+    public struct CreatureValuesChangedEvent: IEvent
+    {
+        public CreatureController CreatureController { get; private set; }
+        public int NewHPValue { get; private set; }
+        public int NewWetValue { get; private set; }
+
+        public CreatureValuesChangedEvent(CreatureController creatureController, int newHP, int newWet)
+        {
+            CreatureController = creatureController;
+            NewHPValue = newHP;
+            NewWetValue = newWet;
+        }
+    }
+
     public struct CreatureParticleCollisionEvent: IEvent
     {
         public CreatureController ThisCreatureController { get; private set; }
